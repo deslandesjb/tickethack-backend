4faaa759c8469077fac50c2fcd415ea2552bfbfc
Jb <deslandes.jb@gmail.com>
2025-11-04 14:10:53 +0100

    trips OK - require departure arrival and show only trips later than the date selected

--- routes/trips.js
+++ routes/trips.js
@@ -8,29 +8,32 @@ const Trip = require("../models/trips");
 /* GET trips listing. */
 router.get("/", (req, res) => {
   Trip.find().then((data) => {
-    // console.log(typeof data[0].date); // object
     res.json({trips: data});
   });
 });
-
-const date = Date.now();
-console.log(date);
-console.log(typeof date);
 
 /* GET only trips filtered. */
 router.post("/", (req, res) => {
   const cityDeparture = req.body.departure;
   const cityArrival = req.body.arrival;
-  if (!cityDeparture || !cityArrival) {
+  let reqCityDate = new Date(req.body.date);
+  reqCityDate = Date.parse(reqCityDate);
+  if (!cityDeparture || !cityArrival || !reqCityDate) {
     res.json({result: false, error: "Missing or empty fields"});
   } else {
     Trip.find({departure: cityDeparture, arrival: cityArrival}).then((data) => {
-      const date = Date.now();
-      console.log(date);
       if (data) {
-        console.log(data[0].date);
-        console.log(typeof data[0].date);
-        res.json({trips: data});
+        let arr = [];
+        for (let i = 0; i < data.length; i++) {
+          // convert each data.date into number
+          let dataDate = data[i].date;
+          dataDate = Date.parse(dataDate);
+          if (reqCityDate <= dataDate) {
+            arr.push(data[i]);
+          }
+        }
+
+        res.json({trips: arr});
       } else {
         res.json({result: false, error: "Trips not found"});
       }
