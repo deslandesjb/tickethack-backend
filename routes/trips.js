6660271532f5b6cc94e1506f1a4fd15042a4e9e7
Jb <deslandes.jb@gmail.com>
2025-11-04 12:33:52 +0100

    connection ok and post trips setup

--- routes/trips.js
+++ routes/trips.js
@@ -13,10 +13,29 @@ router.get("/", (req, res) => {
   });
 });
 
+const date = Date.now();
+console.log(date);
+console.log(typeof date);
+
 /* GET only trips filtered. */
-router.get("/:cityDeparture", (req, res) => {
-  Trip.find().then((data) => {
-    res.json({trips: data});
-  });
+router.post("/", (req, res) => {
+  const cityDeparture = req.body.departure;
+  const cityArrival = req.body.arrival;
+  if (!cityDeparture || !cityArrival) {
+    res.json({result: false, error: "Missing or empty fields"});
+  } else {
+    Trip.find({departure: cityDeparture, arrival: cityArrival}).then((data) => {
+      const date = Date.now();
+      console.log(date);
+      if (data) {
+        console.log(data[0].date);
+        console.log(typeof data[0].date);
+        res.json({trips: data});
+      } else {
+        res.json({result: false, error: "Trips not found"});
+      }
+    });
+  }
 });
+
 module.exports = router;
